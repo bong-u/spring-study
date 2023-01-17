d9fcb62117985f8963894792b6da0a898efcd105
bong-u <bongudev@gmail.com>
2023-01-17 15:40:42 +0900

    🐛 Fix : 404 ERROR

--- sp-ex05/src/main/java/config/MvcConfig.java
+++ sp-ex05/src/main/java/config/MvcConfig.java
@@ -17,6 +17,6 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.jsp("/WEB-INF/view", ".jsp");
+        registry.jsp("/WEB-INF/view/", ".jsp");
     }
 }
