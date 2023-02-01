4c205a2a3485e360b05758ec04ae84d1ee9326f3
bong-u <bongudev@gmail.com>
2023-02-01 10:41:03 +0900

    📝 Update

--- sp-ex06/src/main/java/controller/RegisterController.java
+++ sp-ex06/src/main/java/controller/RegisterController.java
@@ -2,7 +2,6 @@ package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,11 +28,10 @@ public class RegisterController {
 
     @PostMapping("/register/step2")
     public String handleStep2(
-    @RequestParam(value="agree", defaultValue="false") Boolean agree, Model model) {
+    @RequestParam(value="agree", defaultValue="false") Boolean agree, RegisterRequest registerRequesst) {
         if (!agree) {
             return "register/step1";            
         }
-        model.addAttribute("registerRequest", new RegisterRequest());
         return "register/step2";
     }
 
--- sp-ex06/src/main/java/controller/SurveyController.java
+++ sp-ex06/src/main/java/controller/SurveyController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import survey.AnsweredData;
 import survey.Question;
@@ -17,10 +18,12 @@ import survey.Question;
 @RequestMapping("/survey")
 public class SurveyController {
     @GetMapping
-    public String form(Model model) {
+    public ModelAndView form(Model model) {
         List<Question> questions = createQuestions();
-        model.addAttribute("questions", questions);
-        return "survey/surveyForm";
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("questions", questions);
+        mav.setViewName("survey/surveyForm");
+        return mav;
     }
     
     private List<Question> createQuestions() {
